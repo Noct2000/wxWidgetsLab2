e9188b32f25ac98f52abbe27a5ee58121bbe322f
Noct2000 <oleg.kvasha.00@gmail.com>
2023-11-02 15:38:34 +0200

    update wxGraphData class

--- wxGraphData.cpp
+++ wxGraphData.cpp
@@ -1,16 +1,143 @@
 #include "wxGraphData.h"
+#include <wx/file.h>
+
+#include <wx/arrimpl.cpp>
+WX_DEFINE_OBJARRAY(wxVectorRealPoint);
 
 wxGraphData::wxGraphData()
 {
-    //ctor
+    m_pts = new wxVectorRealPoint();
+    m_x_border = 0;
 }
 
 wxGraphData::~wxGraphData()
 {
-    //dtor
+    delete m_pts;
 }
 
 wxGraphData::wxGraphData(const wxGraphData& other)
 {
-    //copy ctor
+    m_pts = new wxVectorRealPoint();
+    m_x_border = other.GetXBorder();
+    for (int i = 0; i < other.GetCount(); i++)
+    {
+        m_pts->Add(other.GetPoint(i));
+    }
+}
+
+void wxGraphData::Clear()
+{
+    m_pts->Clear();
+    m_x_border = 0;
+}
+
+bool wxGraphData::IsEmpty() const
+{
+    return GetCount() == 0;
+}
+
+int wxGraphData::GetCount() const
+{
+    return m_pts->GetCount();
+}
+
+void wxGraphData::AddPoint(double x, double y)
+{
+    m_pts->Add(wxRealPoint(x, y));
+}
+
+const wxRealPoint& wxGraphData::GetPoint(int index) const
+{
+    return (const wxRealPoint&)m_pts->Item(index);
+}
+
+const wxRealPoint& wxGraphData::operator[](int index)
+{
+    return m_pts->Item(index);
+}
+
+double wxGraphData::GetXBorder() const
+{
+    return m_x_border;
+}
+
+bool wxGraphData::SaveToFile(const wxString& filename) const
+{
+    wxFile file;
+    if (!file.Open(filename, wxFile::write))
+    {
+        return false;
+    }
+
+    bool res = file.Write(&m_x_border, sizeof(double));
+    if (!res)
+    {
+        file.Close();
+        return false;
+    }
+
+    for (int i = 0; i < GetCount(); i++)
+    {
+        double x = GetPoint(i).x;
+        double y = GetPoint(i).y;
+
+        res = file.Write(&x, sizeof(double));
+        if (!res)
+        {
+            file.Close();
+            return false;
+        }
+
+        res = file.Write(&y, sizeof(double));
+        if (!res)
+        {
+            file.Close();
+            return false;
+        }
+    }
+
+    file.Close();
+    return true;
+}
+
+bool wxGraphData::LoadFromFile(const wxString& filename)
+{
+    wxFile file;
+    if (!file.Open(filename, wxFile::read))
+    {
+        return false;
+    }
+
+    bool res = file.Read(&m_x_border, sizeof(double));
+    if (!res)
+    {
+        file.Close();
+        return false;
+    }
+
+    while (!file.Eof())
+        {
+            double x = 0, y = 0;
+            if (!(res = file.Read(&x, sizeof(double)))) break;
+            if (!(res = file.Read(&y, sizeof(double)))) break;
+            AddPoint(x, y);
+        }
+
+    file.Close();
+    return true;
+}
+
+void wxGraphData::Generate(MATH_FUNC periodic_math_func, double x_border)
+{
+    m_x_border = x_border;
+    double step_x = 0.01;
+
+
+    for (double x = -x_border * M_PI; x <= x_border * M_PI; x += step_x)
+    {
+        double y1 = periodic_math_func(x);
+        double y2 = periodic_math_func(x + step_x);
+        this->AddPoint(x, y1);
+        this->AddPoint(x + step_x, y2);
+    }
 }
--- wxGraphData.h
+++ wxGraphData.h
@@ -1,17 +1,35 @@
 #ifndef WXGRAPHDATA_H
 #define WXGRAPHDATA_H
 
+#include <wx/dynarray.h>
+#include <wx/gdicmn.h>
+
+WX_DECLARE_OBJARRAY(wxRealPoint, wxVectorRealPoint);
+
+typedef double (*MATH_FUNC)(double);
 
 class wxGraphData
 {
-    public:
-        wxGraphData();
-        virtual ~wxGraphData();
-        wxGraphData(const wxGraphData& other);
+public:
+    wxGraphData();
+    virtual ~wxGraphData();
+    wxGraphData(const wxGraphData& other);
+    void Clear();
+    bool IsEmpty() const;
+    int GetCount() const;
+    void AddPoint(double x, double y);
+    const wxRealPoint& GetPoint(int index) const;
+    const wxRealPoint& operator[] (int index);
+    bool SaveToFile(const wxString& filename) const;
+    bool LoadFromFile(const wxString& filename);
+    void Generate(MATH_FUNC periodic_math_func = sin, double x_border = 4.);
+    double GetXBorder() const;
 
-    protected:
+protected:
 
-    private:
+private:
+    double m_x_border;
+    wxVectorRealPoint* m_pts;
 };
 
 #endif // WXGRAPHDATA_H
